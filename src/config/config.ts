037c421c7e166f325339fddfc9cb0e16f9ff216a
coefvo <verticalorigin.com>
2020-12-15 19:02:18 +0100

    jwt section

--- src/config/config.ts
+++ src/config/config.ts
@@ -15,5 +15,8 @@ export const config = {
     "database": process.env.POSTGRES_DATABASE,
     "host": process.env.POSTGRES_HOST,
     "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": "helloworld"
   }
 }
